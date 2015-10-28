269e498278d59cdc3f99255e33bcaad4cb66bab4
snekkje <jkgulliksen@gmail.com>
2015-10-29 10:02:52 +1300

    first commit

---
+++ best.R
@@ -0,0 +1,17 @@
+best <- function(state, outcome) {
+  if(outcome == "heart attack"){
+    colName <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
+  } else if (outcome == "heart failure") {
+    colName <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
+  } else if (outcome == "pneumonia") {
+    colName <- "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
+  } else stop("invalid outcome")
+  
+  data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  s <- split(data, data$State)
+  
+  z <- lapply(s, function(x) x$"Hospital.Name"[as.numeric(x[[colName]]) ==
+                                                 min(as.numeric(x[[colName]]), na.rm = TRUE)] )
+  
+  z[[state]][!is.na(z[[state]])]
+}
