2753e912efd39b9d92fd18e66234b574cac04cc3
snekkje <jkgulliksen@gmail.com>
2015-10-29 07:29:21 +0900

    finished "best" function

--- best.R
+++ best.R
@@ -6,12 +6,18 @@ best <- function(state, outcome) {
   } else if (outcome == "pneumonia") {
     colName <- "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
   } else stop("invalid outcome")
-  
+
   data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-  s <- split(data, data$State)
-  
-  z <- lapply(s, function(x) x$"Hospital.Name"[as.numeric(x[[colName]]) ==
-                                                 min(as.numeric(x[[colName]]), na.rm = TRUE)] )
-  
-  z[[state]][!is.na(z[[state]])]
+
+  if (state %in% data$State) {
+    s <- split(data, data$State)
+
+    hospitalList <- lapply(s, function(x) x$"Hospital.Name"[as.numeric(x[[colName]]) ==
+                                                   min(as.numeric(x[[colName]]), na.rm = TRUE)] )
+
+    sortedHospitalList <- sort(hospitalList[[state]][!is.na(hospitalList[[state]])])
+    sortedHospitalList[1]
+
+  } else stop("invalid state")
+
 }
