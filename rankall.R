349e324bacf281a6574ddb89838bd97a336aea05
snekkje <jkgulliksen@gmail.com>
2015-10-30 01:16:15 +1300

    work on the "rankall" function

--- rankall.R
+++ rankall.R
@@ -1,7 +1,20 @@
 rankall <- function(outcome, num = "best") {
-  ## Read outcome data
-  ## Check that state and outcome are valid
-  ## For each state, find the hospital of the given rank
-  ## Return a data frame with the hospital names and the
-  ## (abbreviated) state name
+  if(outcome == "heart attack"){
+    colName <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
+  } else if (outcome == "heart failure") {
+    colName <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
+  } else if (outcome == "pneumonia") {
+    colName <- "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
+  } else stop("invalid outcome")
+  
+  data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+
+  s <- split(data, data$State)
+  #hospitalList <- lapply(s, function(x) x$"Hospital.Name"[!is.na()] )
+  hospitalList <- lapply(s, function(x) x$"Hospital.Name"[num] )
+  #remove hospitals that do not have data for required oucome
+  
+  data.frame(hospital = unlist(hospitalList), state = names(hospitalList))
+  
+  
 }
